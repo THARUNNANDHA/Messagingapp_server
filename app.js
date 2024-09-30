6ffeecd60990180014c05e723429b1bcafd1fd24
TharunNandhakumar <tharun.nandhakumar@openturf.in>
2024-09-30 12:21:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const io = new Server(
     server,
     {
         cors: {
-            origin: 'http://localhost:3000'
+            origin: ['http://localhost:3000','https://messagingappserver-production.up.railway.app/']
         }
     }
 )
